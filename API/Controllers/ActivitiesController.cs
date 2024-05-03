3242eed06808347c8c83a7b88cc856bae0b27628
Mjolnir <utsabsingh5@gmail.com>
2024-05-03 21:57:27 +0545

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Reflection.Metadata.Ecma335;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //api/activities
+        public async Task<ActionResult<IEnumerable<Activity>>> GetActivities()
+        {
+            
+            return await _context.Activities.ToListAsync();
+
+        }
+
+        [HttpGet("{id}")] //api/activities
+        public async Task<ActionResult<Activity>> GetActivities(Guid id)
+        {
+
+            return await _context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+
+        }
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+       
+        
+    }
+}
