3f97fd044e9f680a3c6d1cb50ff6c934661c97ee
Mjolnir <utsabsingh5@gmail.com>
2024-05-07 14:25:10 +0545

    added CRUD API

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest<Unit> // Update to implement IRequest<Unit>
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Unit> // Update the interface to IRequestHandler<Command, Unit>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity);
+                await _context.SaveChangesAsync();
+                return Unit.Value;
+            }
+        }
+    }
+}
